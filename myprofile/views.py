216d1aecabca2a7a3bdf7c0f45dde11e943dec3a
keiji <BB123021@sun.ac.jp>
2025-01-12 17:21:32 +0900

    Create home page

--- myprofile/views.py
+++ myprofile/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    return render(request, 'myprofile/home.html')
+
+def activity(request):
+    return render(request, 'myprofile/activity.html')
